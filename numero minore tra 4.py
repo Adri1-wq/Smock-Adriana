5eb610a48bc4cf1530ffa88743730593f44d50eb
adriana.sck <smockadriana10@gmail.com>
2025-12-04 08:37:00 +0100

    define-a-function-for-finding-minimum

--- numero minore tra 4.py
+++ numero minore tra 4.py
@@ -1,22 +1,28 @@
+#dati 4 numeri in ingresso resituire il minore di 4
+
+def minore(a,b,c,d):
+ min = 0  
+ if a<b:
+      if a<c:
+        if a<d:
+            min = a
+        else:
+            min = d
+      else:
+        if c<d:
+            min = c
+        else: 
+            min = d
+ else:
+     if b<c:
+        if b<d:
+            min = b
+ return min
 
 a = float(input("inserisci a: "))
 b = float(input("inserisci b: "))
 c = float(input("inserisci c: "))
 d = float(input("inserisci d: "))
 
-if a<b:
-    if a<c:
-        if a<d:
-            min = a
-        else:
-            min = d
-    else:
-        if c<d:
-            min = c
-        else: 
-            min = d
-else:
-    if b<c:
-        if b<d:
-            min = b
-print ("tra ", a,",",b, ",",c,"e",d, " il minore è ",min)
+m = minore(a,b,c,d)
+print ("tra ", a,",",b, ",",c,"e",d, " il minore è ",m)
